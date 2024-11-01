566cc92790296fcb8bc7bb3df2c4a52d649096d6
Sandra Rumer <rumersandra@gmail.com>
2024-11-01 16:32:21 +0100

    inspection ritus

--- Inspection ritus.js
+++ Inspection ritus.js
@@ -3,7 +3,6 @@ if (typeof timeoutHandle === 'undefined') {
   var timeoutHandle
 }
 
-//to Do : large Dildo
 
 // -----------------------------------------------------------------------------------------------
 
@@ -23,16 +22,30 @@ inspectionList = [
   "ItemNipples",
   "ItemArms",
   "ItemArms",
+  "ItemHands",
   "ItemTorso",
   "ItemTorso2",
   "ItemPelvis",
   "ItemVulva",
+  "ItemVulva",
+  "ItemVulva",
+  "ItemPenis",
   "ItemVulvaPiercings",
+  "ItemVulvaPiercings",
+  "ItemVulvaPiercings",
+  "ItemButt",
+  "ItemButt",
   "ItemButt",
   "ItemLegs",
   "ItemFeet",
   "ItemFeet",
-  "ItemFeet"
+  "ItemFeet",
+  "ItemBoots",
+  "ItemBoots",
+  "ItemBoots",
+  "ItemHands",
+  "ItemBoots",
+  "ItemMouth"
 ]
 t =
   [
@@ -40,45 +53,78 @@ t =
     "ItemDevices"
   ]
 
-
-actionList = [
-  "TakeCare",//had/ face
-  "Caress", //had/ face
-  "Caress",//nose 
-  "Caress", //ears //
-  "Caress", //ears  "Whisper", //ears
-  "Caress",//mouth 
-  "Caress",//mouthtodo "Kiss", //mouth
-  "Caress",// "PenetrateSlow", //mouth todo penetrate   
-  "Caress",//neck
-  "MassageHands",//neck
-  "Caress",//breast
-  "Caress",//nippls
-  "Caress",//niples
-  "Caress", //arms
-  "MassageHands",//arms
-  "Caress",  //Torso
-  "MassageHands",  //Torso"MassageHands",  //Torso
-  "Caress", //"MassageHands",  //Pelvis
-  "MasturbateItem", //"PenetrateSlow", // Vulva
-  "Caress", // "PenetrateSlow", //Clitoris
-  "Spank", //butt
-  "Caress",  //Legs
-  "Caress",  //feet
-  "Tickle",  //feet
-  "MassageHands" //feet
+  inspectionList = [
+    
+    "ItemVulva",
+  "ItemVulva",
+  "ItemVulva",
+  "ItemPenis",
+  "ItemVulvaPiercings",
+  "ItemVulvaPiercings",
+  "ItemVulvaPiercings",
+  "ItemButt",
+  "ItemButt",
+  "ItemButt"
 ]
 
 
 
+actionList = [
+   "TakeCare",//had/ face
+  "Caress", //had/ face
+  "Caress",//nose 
+  "Caress", //ears //
+  "Whisper", //ears  "Whisper", //ears
+  "Caress",//mouth 
+  "Kiss",//mouthtodo "Kiss", //mouth
+  "PenetrateSlow", //mouth 
+  "Caress",//neck
+  "MassageHands",//neck
+  "Caress",//breast
+  "Caress",//nippls
+  "Lick",//niples
+  "Caress", //arms
+  "MassageHands",//arms
+  "Caress", //hands
+  "Caress",  //Torso
+  "MassageHands",  //Torso"MassageHands",  //Torso
+  "Caress", //Pelvis
+  "Caress", //Vulva
+  "PenetrateSlow", //Vulva 
+  "MasturbateItem", //"PenetrateSlow", // Vulva
+  "Caress", //Penis 
+  "Caress", // "PenetrateSlow", //Clitoris
+  "Slap", ////Clitoris
+  "MasturbateItem", //Clitoris
+  "Caress",  //butt
+  "Spank", //butt
+  "PenetrateSlow", //butt
+  "Caress",  //Legs
+  "Caress",  //feet
+  "Tickle",  //feet
+  "MassageHands", //feet
+  "Caress",  //boots
+  "MassageHands", //boots
+  "Tickle",  //boots
+  "TakeCare",//boots
+  "TakeCare",//arms
+  "frenchKiss"
+]
+actionList= [
+  "Caress", //Vulva
+  "PenetrateSlow", //Vulva 
+  "MasturbateItem", //"PenetrateSlow", // Vulva
+  "Caress", //Penis 
+  "Caress", // "PenetrateSlow", //Clitoris
+  "Slap", ////Clitoris
+  "MasturbateItem", //Clitoris
+  "Caress",  //butt
+  "Spank", //butt
+  "PenetrateSlow" //butt
+  
+]
 
 s = [
-
-
-
-
-
-
   "Spank", "Tickle", "Caress", "Caress"]
 timeoutFactor = 30
 timeoutFactor = 5
@@ -167,7 +213,7 @@ function prepareInspection() {
   ChatRoomCharacterUpdate(Player)
 
   for (var D = 0; D < ChatRoomCharacter.length; D++) {
-    if (ChatRoomCharacter[D].MemberNumber != Player.MemberNumber && !ChatRoomCharacter[D].IsOwned() && (ReputationCharacterGet(ChatRoomCharacter[D], "Dominant") < 50)) {
+    if (ChatRoomCharacter[D].MemberNumber != Player.MemberNumber && !ChatRoomCharacter[D].IsOwned() && (ReputationCharacterGet(ChatRoomCharacter[D], "Dominant") < 89)) {
       memorizeClothing(ChatRoomCharacter[D])
       delinquent = ChatRoomCharacter[D]
       removeClothes(ChatRoomCharacter[D], false, false)
@@ -182,65 +228,99 @@ function prepareInspection() {
 }
 
 function performInspection() {
+  playerList = []
   for (var D = 0; D < ChatRoomCharacter.length; D++) {
-    if (ChatRoomCharacter[D].MemberNumber != Player.MemberNumber && (ReputationCharacterGet(ChatRoomCharacter[D], "Dominant") < 50)) {
+    if (ChatRoomCharacter[D].MemberNumber != Player.MemberNumber && (ReputationCharacterGet(ChatRoomCharacter[D], "Dominant") < 89)) {
       delinquent = ChatRoomCharacter[D]
-      removeClothes(delinquent, true, false)
       count++
-      ServerSend("ChatRoomChat", { Content: "Performing Inspection for " + ChatRoomCharacter[D].Name, Type: "Chat" });
+      playerList.push(ChatRoomCharacter[D])
+      ServerSend("ChatRoomChat", { Content: "Inspection candidate: " + ChatRoomCharacter[D].Name, Type: "Chat" });
       //setTimeout(actionStep (delinquent, 0),800*1000)
-      ChatRoomCharacterUpdate(delinquent)
-      setTimeout(function (Player) { actionStep(delinquent, 0) }, timeoutFactor * 1000, Player)
-      //nextStep( delinquent ,0)
+       //nextStep( delinquent ,0)
       //  for (var inspectionStep = 0; inspectionStep  < inspectionList.length; inspectionStep ++)
       //  {
       //    timeoutHandle = setTimeout(actionStep (delinquent, inspectionStep),inspectionStep*100*1000)
 
       //  }
-
     }
     else
       console.log(ChatRoomCharacter[D].MemberNumber + " " + ChatRoomCharacter[D].Name + " Inspection not possible")
   }
+  setTimeout(function (Player) { performSingleInspection(playerList) }, timeoutFactor * 1000, Player)
+}
 
+//function performSingleInspection()//
+//{
+//  conole.log("performSingleInspection function")
+//  performSingleInspection(playerList) 
+//} 
 
+function performSingleInspection(playerList) {
+console.log("performSingleInspection" + playerList.length)
+  if (playerList.length > 0) {
+    actdelinquent = playerList.shift()
+    removeClothes(actdelinquent, true, false)
+    ChatRoomCharacterUpdate(actdelinquent)
+    ServerSend("ChatRoomChat", { Content: "Inspection is performed on:" + actdelinquent.Name, Type: "Chat" });
+     setTimeout(function (Player) { actionStep(actdelinquent, 0, playerList) }, timeoutFactor * 1000, Player)
+  }
+  else
+  {
+    setTimeout(function (Player) { finishInspection() }, timeoutFactor * 6000, Player)
+    }
 }
 
 
-function actionStep(char, step) {
+function actionStep(char, step, playerList) {
   console.log(step)
-  targetGroup = ActivityGetGroupOrMirror(Player.AssetFamily, inspectionList[step])
-  activity = ActivityAllowedForGroup(Player, inspectionList[step]).find(function (obj) {
+  console.log("actionStep " + playerList.length)
+  if (char.HasPenis()) {
+    if (inspectionList[step] == "ItemVulva") {
+      step++
+      nextStep(char, step, playerList)
+      return
+    }
+  }
+  if (char.HasVagina()) {
+    if (inspectionList[step] == "ItemPenis") {
+      step++
+      nextStep(char, step, playerList)
+      return
+    }
+  }
+  targetGroup = ActivityGetGroupOrMirror(char.AssetFamily, inspectionList[step])
+  activity = ActivityAllowedForGroup(char, inspectionList[step]).find(function (obj) {
     return obj.Activity.Name == actionList[step];
   })
   console.log(targetGroup)
   console.log(activity)
   if (activity == null)
-    console.log ("null") 
-    else
-  ActivityRun(Player, char, targetGroup, activity)
+    console.log("null")
+  else
+    ActivityRun(Player, char, targetGroup, activity)
   step++
-  nextStep(char, step)
+  nextStep(char, step,playerList)
 }
 
-function nextStep(delinquent, step) {
+function nextStep(delinquent, step, playerList) {
   clearTimeout(timeoutHandle)
   if (step >= inspectionList.length)
     //timeoutHandle = setTimeout(finishInspection(delinquent), 8000*1000)
-    setTimeout(function (Player) { finishInspection(delinquent) }, timeoutFactor * 1500, Player)
+    setTimeout(function (Player) { finishSingleInspection(delinquent, playerList) }, timeoutFactor * 1500, Player)
   else {
     //timeoutHandle = setTimeout(actionStep (delinquent, step),100*1000)
-    setTimeout(function (Player) { actionStep(delinquent, step) }, timeoutFactor * 1000, Player)
+    setTimeout(function (Player) { actionStep(delinquent, step, playerList) }, timeoutFactor * 1000, Player)
   }
-  function finishInspection(char) {
-    console.log("finish")
-    ServerSend("ChatRoomChat", { Content: "Inspection of   " + char.Name + " done ", Type: "Chat" });
-    ServerSend("ChatRoomChat", { Content: `No weapon, drugs or explosive found!`, Type: "Chat" });
-    InventoryRemove(Player, "ItemHandheld")
-    ServerSend("ChatRoomChat", { Content: `Well done`, Type: "Chat", Target: char.MemberNumber });
-    reapplyClothing(char)
-
+}
+function finishSingleInspection(char,playerList) {
+  console.log("finish")
+  ServerSend("ChatRoomChat", { Content: "Inspection of   " + char.Name + " is over ", Type: "Chat" });
+  ServerSend("ChatRoomChat", { Content: `No hidden weapon, drugs or explosive found!`, Type: "Chat" });
+  ServerSend("ChatRoomChat", { Content: `Well done`, Type: "Chat", Target: char.MemberNumber });
+  reapplyClothing(char)
+  performSingleInspection(playerList)
   }
-
-
+function finishInspection() {
+  InventoryRemove(Player, "ItemHandheld")
+  ServerSend("ChatRoomChat", { Content: "Inspection of all slaves is over ", Type: "Chat" });
 }
